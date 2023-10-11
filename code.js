2560f9c35c8ae7b0db4f4cf0a5a6dc0a79f781d0
gbrown20 <gbrown20@DESKTOP-2J0M7L5.localdomain>
2023-10-11 17:28:56 -0600

    Need to work on runtime analysis

--- code.js
+++ code.js
@@ -1,39 +1,36 @@
-function mergesort(arr) {
-    if (arr.length <= 1 || arr == null) {
-        return arr;
+function mergesort(array) {
+    if (array == null || array.length <= 1) {
+        return array;
     }
 
-    const len = arr.length;
+    const len = array.length;
     const aux = new Array(len);
 
     for (let size = 1; size < len; size *= 2) {
         for (let start = 0; start < len; start += 2 * size) {
-            const left = Math.min(start + size, len);
-            const right = Math.min(start + 2 * size, len);
-            merge(arr, aux, start, mid, end);
+            const mid = Math.min(start + size, len);
+            const end = Math.min(start + 2 * size, len);
+
+            let leftIndex = start;
+            let rightIndex = mid;
+
+            for (let i = start; i < end; i++) {
+                aux[i] = array[i];
+            }
+
+            for (let i = start; i < end; i++) {
+                if (leftIndex < mid && (rightIndex >= end || aux[leftIndex] <= aux[rightIndex])) {
+                    array[i] = aux[leftIndex];
+                    leftIndex++;
+                } else {
+                    array[i] = aux[rightIndex];
+                    rightIndex++;
+                }
+            }
         }
     }
 
-    return arr;
-}
-
-function merge(arr, aux, start, mid, end) {
-    let leftIndex = start;
-    let rightIndex = mid;
-
-    for (let i = start; i < end; i++) {
-        aux[i] = arr[i];
-    }
-
-    for (let i = start; i < end; i++) {
-        if (leftIndex < mid && (rightIndex >= end || aux[leftIndex] <= aux[rightIndex])) {
-            arr[i] = aux[leftIndex];
-            leftIndex++;
-        } else {
-            arr[i] = aux[rightIndex];
-            rightIndex++;
-        }
-    }
+    return array;
 }
 
 
